2d464c8bf736e8ebf5c77e4b45c6310dab6f2b5f
Abdou Rachad BAWA <bawarachad983@gmail.com>
2025-07-01 21:29:18 +0100

    configuration et mise preparation backend

---
+++ backend/routes/sms.route.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+// Définissez vos routes ici
+router.get('/', (req, res) => {
+  // Votre logique
+});
+
+// Exportez DIRECTEMENT le router
+module.exports = router;
